fc55de48f4f3511429ec46b9bfd51bd57257726b
Gabriel <122925386+gabifrez@users.noreply.github.com>
2026-06-18 12:11:34 +0300

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        dictionary = {}
+        values = []
+        for point in points:
+            distance = math.sqrt(point[0]**2 + point[1]**2)
+            distance = round(distance, 2)
+            if distance in dictionary:
+                dictionary[distance].append(point)
+            else:
+                values.append(distance)
+                dictionary[distance] = [point]
+        values.sort()
+        answer = []
+        for value in values:
+            for point in dictionary[value]:
+                answer.append(point)
+                k-=1
+            if k == 0:
+                break
+        return answer
+        
+
