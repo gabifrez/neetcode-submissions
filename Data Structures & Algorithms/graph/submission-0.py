ce2bf0aa28db39fa6846885a0d763c71ad44f633
Gabriel <122925386+gabifrez@users.noreply.github.com>
2026-08-24 18:52:54 +0300

    Add: graph - submission-0

---
+++ Data Structures & Algorithms/graph/submission-0.py
@@ -0,0 +1,33 @@
+
+class Graph:
+    
+    def __init__(self):
+        self.graph = {}
+
+    def addEdge(self, src: int, dst: int) -> None:
+        if src not in self.graph:
+            self.graph[src] = set()
+        if dst not in self.graph:
+            self.graph[dst] = set()
+        self.graph[src].add(dst)
+    def removeEdge(self, src: int, dst: int) -> bool:
+        if src not in self.graph or dst not in self.graph[src]:
+            return False
+        self.graph[src].remove(dst)
+        return True
+    def hasPath(self, src: int, dst: int) -> bool:
+        queue = deque()
+        visited = []
+        queue.append(src)
+        visited.append(src)
+        while len(queue):
+            for i in range(len(queue)):
+                node = queue.popleft()
+                if node == dst:
+                    return True
+                for conexion in self.graph[node]:
+                    if conexion not in visited:
+                        visited.append(conexion)
+                        queue.append(conexion)
+        return False
+
